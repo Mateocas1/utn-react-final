4516861cf5090f42fc5103dc8786822dc42e044c
Mateo <castiglionemateo1@gmail.com>
2026-02-06 02:29:34 -0300

    feat: setup inicial del proyecto con Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Gestión de Productos</h1>
+      <p>Aplicación en construcción...</p>
+    </div>
+  )
+}
+
+export default App
